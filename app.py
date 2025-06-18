31793a9fc5f71edf85d763cc7d29eec149099bdc
ganyiwatakunda <118890097+ganyiwatakunda@users.noreply.github.com>
2025-06-18 14:13:13 +0200

    Update app.py
    changed way of saving the client file

--- app.py
+++ app.py
@@ -28,8 +28,9 @@ def generate_exam_response(role: str, subject: str, prompt: str) -> str:
 
     from openai import OpenAI
     #embeddings = OpenAIEmbeddings(model=EMBEDDING_MODEL)
-    #client = OpenAI()  # no proxies passed
-    embeddings = OpenAIEmbeddings(model=EMBEDDING_MODEL,client=OpenAI())  # ensures compatible instantiation with no unexpected proxies )
+    client = OpenAI()  # no proxies passed
+    #embeddings = OpenAIEmbeddings(model=EMBEDDING_MODEL,client=OpenAI())  # ensures compatible instantiation with no unexpected proxies )
+    embeddings = OpenAIEmbeddings(model=EMBEDDING_MODEL,client=client)  # ensures compatible instantiation with no unexpected proxies )
 
     if os.path.exists(STORAGE_PATH):
         vectorstore = FAISS.load_local(STORAGE_PATH, embeddings, allow_dangerous_deserialization=True)
